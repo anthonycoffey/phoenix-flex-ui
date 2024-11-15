4d1ac5dac6f807d67f471e05e68999053147b562
Anthony Coffey <coffey.j.anthony@gmail.com>
2024-11-15 06:55:51 -0600

    cleaning up main app file

--- src/PhoenixFlexUiPlugin.js
+++ src/PhoenixFlexUiPlugin.js
@@ -10,24 +10,6 @@ export default class PhoenixFlexUiPlugin extends FlexPlugin {
   }
 
   async init(flex, manager) {
-    console.log("flex", { flex });
-
-    // manager.strings.TaskInfoPanelContent = `
-    //   <h1>CUSTOMER CONTEXT</h1>
-    //   <p>Customer name / phone number: {{task.attributes.name}}</p>
-    //   <h1>TASK CONTEXT</h1>
-    //   <h2>Task type</h2>
-    //   <p>{{task.attributes.title}}</p>
-    //   <p>{{task.title}}</p>
-    //   <h2>Task created on</h2>
-    //   <p>{{task.dateCreated}}</p>
-    //   <h2>Task priority</h2>
-    //   <p>{{task.priority}}</p>
-    //   <h2>Task queue</h2>
-    //   <p>{{task.taskQueueName}}</p>
-    //   <hr />
-    //   `;
-
     manager.strings.TaskExtraInfo =
       "Time since last update: {{helper.durationSinceUpdate}}";
 
@@ -37,8 +19,8 @@ export default class PhoenixFlexUiPlugin extends FlexPlugin {
 
     flex.CRMContainer.defaultProps.uriCallback = (task) => {
       console.log({ task });
-      console.log(task?.attributes?.conference);
-      console.log(task?.attributes?.conversations);
+      console.log("conference", task?.attributes?.conference);
+      console.log("conversations", task?.attributes?.conversations);
 
       let url = "https://localhost:8888/search";
       if (task && task.attributes) {
